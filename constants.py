a0eedac74bfdf6aebc700c83ba58b85b6d0dbb57
Greg Mattson <gregwmattson@gmail.com>
2025-09-28 16:43:50 -0500

    Initial local commit

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+NICE_BLUE = (106, 159, 181)
+
+# Setting the window size and title
+SCREEN_SIZE = (1200, 800)
+WINDOW_TITLE = "Pokemon Battle!"
+
+# Set the frame rate
+FRAME_RATE = 60  # 60 frames per second
