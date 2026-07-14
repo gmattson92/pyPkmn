d4441132a5c47f04d10c4accd07fa8ca49c50f45
Greg Mattson <gregwmattson@gmail.com>
2026-07-14 17:29:28 -0500

    Cleaned up TurnManager code, particularly before_move(), for much better readability and organization.

--- turnmanager.py
+++ turnmanager.py
@@ -13,7 +13,7 @@ class TurnManager:
         - what changes need to be made to various status flags and counters,
             and applying those changes
     Most changes to status counters are applied in before_move(). The exception
-    is trapping/binding, which changes in Battle.end_round().
+    is trapping/binding, which changes in RoundManager.end_round().
     """
 
     def __init__(self, mu):
@@ -37,68 +37,161 @@ class TurnManager:
     def trap_damage(self, val):
         self._trap_damage = val
 
-    def fully_prz(self, pkmn):
+    def before_move(self, user, other):
+        """
+        Determines whether a Pokemon can use its chosen move this turn, and
+        applies certain changes to Pokemon's status --
+            - decrementing the Sleep counter & checking for expiration
+            - checking for full paralysis
+            - resetting flinch and Hyper Beam recharge flags
+            - decrementing the Confusion counter & checking for expiration
+            - decrementing the Disable counter & checking for expiration
+            - checking if Pokemon hits itself in confusion
+            - decrementing the Multiturn counter & checking for expiration,
+                or resetting if Pokemon hit itself in confusion
+        Note that the Trapping/Trapped counters decrement during end_round,
+        rather than here like all the other counters; likewise for the Toxic
+        counter incrementing. However, Trap counters can be reset here
+        depending on other status effects.
+
+        Returns dict with overall can/can't move determination and details on
+        which checks pass and fail. Valid keys are:
+            - 'can_move'
+            - 'fully_przd'
+            - 'asleep_frozen'
+            - 'flinched'
+            - 'recharging'
+            - 'trap_active'
+            - 'hit_self'
+        """
+        bm_dict = {}
+        update_fns = [self.update_major_status,
+                      self.update_flinch,
+                      self.update_recharge,
+                      self.update_trap_active,
+                      self.update_disable,
+                      self.update_confusion]
+        for fn in update_fns:
+            fn(user, bm_dict)
+
+        # Overall can/can't move
+        can_move = self.can_move(bm_dict)
+        bm_dict['can_move'] = can_move
+
+        # Check for pkmn hitting itself in confusion (but skip this if pkmn
+        # can't move for another reason)
+        hit_self = False
+        if can_move:
+            hit_self = self.confusion_check(user, other)
+        bm_dict['hit_self'] = hit_self
+
+        self.update_multiturn(user, bm_dict)
+
+        return bm_dict
+
+    def after_move(self, user, other):
+        """
+        Apply effects that occur after using a move:
+            - Recurring damage (burn, poison/toxic, leech seed)
+        """
+        self.apply_all_recurring_damage(user, other)
+
+    def update_fully_przd(self, pkmn, bm_dict):
         """
         Checks if pkmn is fully paralyzed and prevented from moving this turn.
-        If so, posts message. Returns bool.
+        If so, posts message. Updates bm_dict with Boolean result.
         """
-        fully_prz = False
+        fully_przd = False
         if pkmn.sm.status == 'PRZ':
-            fully_prz = globals.rng_check(globals.FULL_PRZ_CHANCE)
-            if fully_prz:
+            fully_przd = globals.rng_check(globals.FULL_PRZ_CHANCE)
+            if fully_przd:
                 post_message(f'{pkmn.name} is fully paralyzed!')
-        return fully_prz
+        bm_dict['fully_przd'] = fully_przd
 
-    def can_move_status(self, pkmn):
+    def update_major_status(self, pkmn, bm_dict):
         """
         Checks if pkmn is prevented from moving by a non-volatile status
         condition and, if so, posts the appropriate message. Also handles
-        decrementing the sleep counter.
-        Returns (can_move, asleep_frozen, fully_paralyzed). The latter two are
-        needed for deciding how to handle several other status counters.
+        decrementing the sleep counter. Updates bm_dict with bools for full
+        paralysis, and asleep/frozen.
         """
+        self.update_fully_przd(pkmn, bm_dict)
         asleep_frozen = False
-        fully_prz = False
         if pkmn.sm.status == 'SLP':
-            can_move_status = False
             asleep_frozen = True
             message = pkmn.sm.decrement_counter('sleep')
             post_message(message)
-        elif pkmn.sm.status == 'FRZ':
-            can_move_status = False
+        if pkmn.sm.status == 'FRZ':
             asleep_frozen = True
             post_message(f'{pkmn.name} is frozen solid!')
-        elif pkmn.sm.status == 'PRZ':
-            fully_prz = self.fully_prz(pkmn)
-            if fully_prz:
-                can_move_status = False
-            else:
-                can_move_status = True
-        else:
-            can_move_status = True
+        bm_dict['asleep_frozen'] = asleep_frozen
 
-        return (can_move_status, asleep_frozen, fully_prz)
-
-    def can_move_flinch(self, pkmn):
+    def update_flinch(self, pkmn, bm_dict):
         if pkmn.sm.get_flag('flinch'):
-            can_move_flinch = False
+            flinched = True
             post_message(f'{pkmn.name} flinched!')
             # Reset the flinch flag
             pkmn.sm.reset_flag('flinch')
         else:
-            can_move_flinch = True
-        return can_move_flinch
+            flinched = False
+        bm_dict['flinched'] = flinched
 
-    def can_move_recharge(self, pkmn):
+    def update_recharge(self, pkmn, bm_dict):
         if pkmn.sm.get_flag('recharge'):
-            can_move_recharge = False
+            recharging = True
             post_message(f'{pkmn.name} has to recharge!')
             # Reset the recharge flag, unless pkmn is frozen
             if pkmn.sm.status != 'FRZ':
                 pkmn.sm.reset_flag('recharge')
         else:
-            can_move_recharge = True
-        return can_move_recharge
+            recharging = False
+        bm_dict['recharging'] = recharging
+
+    def update_trap_active(self, pkmn, bm_dict):
+        if pkmn.sm.get_counter('trapped') or pkmn.sm.get_counter('trapping'):
+            trap_active = True
+        else:
+            trap_active = False
+        bm_dict['trap_active'] = trap_active
+
+    def can_move(self, bm_dict):
+        """
+        Checks whether pkmn can move this turn, or is prevented from moving by
+        a major status, flinch, or recharge. Note that trapped/trapping status
+        are **not** included for this purpose, and are handled separately.
+        Returns bool.
+        """
+        can_move = True
+        # Major status
+        if bm_dict['fully_przd'] or bm_dict['asleep_frozen']:
+            can_move = False
+        # Non-status
+        if bm_dict['flinched'] or bm_dict['recharging']:
+            can_move = False
+        return can_move
+
+    def decrement_cnf_dsb(self, bm_dict):
+        """
+        Checks whether to decrement the Confusion and Disable counters, which
+        are paused during sleep, freeze, flinches and recharge turns, but do
+        decrement during full paralysis and trapping turns. Returns bool.
+        """
+        decrement = ((not bm_dict['asleep_frozen'])
+                     and (not bm_dict['flinched'])
+                     and (not bm_dict['recharging']))
+        return decrement
+
+    def update_disable(self, pkmn, bm_dict):
+        if self.decrement_cnf_dsb(bm_dict):
+            if pkmn.sm.get_counter('disable'):
+                msg = pkmn.sm.decrement_counter('disable')
+                post_message(msg)
+
+    def update_confusion(self, pkmn, bm_dict):
+        if self.decrement_cnf_dsb(bm_dict):
+            if pkmn.sm.get_counter('confusion'):
+                msg = pkmn.sm.decrement_counter('confusion')
+                post_message(msg)
 
     def confusion_check(self, user, other):
         """
@@ -150,102 +243,24 @@ class TurnManager:
         damage = min(damage, user.current_hp)
         return damage
 
-    def before_move(self, user, other):
+    def update_multiturn(self, pkmn, bm_dict):
         """
-        Determines whether a Pokemon can use its chosen move this turn, and
-        applies certain changes to Pokemon's status --
-            - decrementing the Sleep counter & checking for expiration
-            - checking for full paralysis
-            - resetting flinch and Hyper Beam recharge flags
-            - decrementing the Confusion counter & checking for expiration
-            - decrementing the Disable counter & checking for expiration
-            - checking if Pokemon hits itself in confusion
-            - decrementing the Multiturn counter & checking for expiration
-        Note that the Trapping/Trapped counters decrement during end_round,
-        rather than here like all the other counters; likewise for the Toxic
-        counter incrementing. However, Trap counters can be reset here
-        depending on other status effects.
-
-        Returns dict with overall can/can't move determination and details on
-        which checks pass and fail. Valid keys are:
-            - 'can_move'
-            - 'asleep_frozen'
-            - 'flinched'
-            - 'recharging'
-            - 'hit_self'
-            - 'trap_active'
+        Multiturn, which is more complicated -- hits in confusion and full
+        paralysis reset the move completely; sleep, freeze and trapped all
+        pause the move but do not decrement the counter.
         """
-        ret = {}
-        # Check non-volatile status and decrement sleep counter
-        can_move_status, asleep_frozen, fully_prz = self.can_move_status(user)
-        ret['asleep_frozen'] = asleep_frozen
-        ret['fully_prz'] = fully_prz
-
-        # Check for flinching
-        can_move_flinch = self.can_move_flinch(user)
-        ret['flinched'] = not can_move_flinch
-
-        # Check for recharge turn
-        can_move_recharge = self.can_move_recharge(user)
-        ret['recharging'] = not can_move_recharge
-
-        # Check for trapping moves
-        if user.sm.get_counter('trapped') or user.sm.get_counter('trapping'):
-            trap_active = True
-        else:
-            trap_active = False
-        ret['trap_active'] = trap_active
-
-        # Overall can/can't move
-        can_move_nonstatus = can_move_flinch and can_move_recharge
-        can_move = can_move_status and can_move_nonstatus
-        ret['can_move'] = can_move
-
-        # Confusion and Disable, which are paused during sleep, freeze,
-        # flinches, and recharge turns, but do decrement during full paralysis
-        # and trapping turns
-        decrement_cnf_dsb = ((not asleep_frozen)
-                             and can_move_flinch
-                             and can_move_recharge)
-        if decrement_cnf_dsb:
-            # Decrement Disable first, because the confusion check happens
-            # right after this
-            if user.sm.get_counter('disable'):
-                msg = user.sm.decrement_counter('disable')
-                post_message(msg)
-            if user.sm.get_counter('confusion'):
-                msg = user.sm.decrement_counter('confusion')
-                post_message(msg)
-
-        # Check for pkmn hitting itself in confusion (but skip this if pkmn
-        # can't move for another reason)
-        hit_self = False
-        if can_move:
-            hit_self = self.confusion_check(user, other)
-        ret['hit_self'] = hit_self
-
-        # Multiturn, which is more complicated -- hits in confusion and full
-        # paralysis reset the move completely; sleep, freeze and trapped all
-        # pause the move but do not decrement the counter.
-        if user.sm.get_counter('multiturn'):
-            reset_mlt = fully_prz or hit_self
+        if pkmn.sm.get_counter('multiturn'):
+            reset_mlt = bm_dict['fully_przd'] or bm_dict['hit_self']
             # Unclear whether flinching also resets Multiturn
-            # reset_mlt = fully_prz or hit_self or (not can_move_flinch)
+            # reset_mlt = (bm_dict['fully_przd'] or bm_dict['hit_self']
+            #              or bm_dict['flinched'])
             if reset_mlt:
-                user.sm.reset_counter('multiturn')
-            decrement_mlt = (not asleep_frozen) and (not trap_active)
+                pkmn.sm.reset_counter('multiturn')
+            decrement_mlt = ((not bm_dict['asleep_frozen'])
+                             and (not bm_dict['trap_active']))
             if decrement_mlt:
-                msg = user.sm.decrement_counter('multiturn')
+                msg = pkmn.sm.decrement_counter('multiturn')
                 post_message(msg)
-
-        return ret
-
-    def after_move(self, user, other):
-        """
-        Apply effects that occur after using a move:
-            - Recurring damage (burn, poison/toxic, leech seed)
-        """
-        self.apply_all_recurring_damage(user, other)
 
     def apply_all_recurring_damage(self, user, other):
         """
